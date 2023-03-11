2bee29d13bb59f09a79a85a5a8752a12baf8a782
AliShamil <elisamilzade@gmail.com>
2023-03-11 20:38:39 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+
+        private Socket udpClient;
+        private IPEndPoint serverEndpoint;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            udpClient = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+        }
+
+        private async void ReceiveScreenshotButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                udpClient.SendTo(new byte[] { 1 }, serverEndpoint);
+                EndPoint remoteEndpoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] receiveBuffer = new byte[1024];
+                var result = await udpClient.ReceiveFromAsync(receiveBuffer, SocketFlags.None, remoteEndpoint);
+                ImageBox.Source = ByteArrayToImage(receiveBuffer);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error receiving screenshot: {ex.Message}");
+            }
+        }
+
+        private BitmapImage ByteArrayToImage(byte[] byteArray)
+        {
+            using (MemoryStream stream = new MemoryStream(byteArray))
+            {
+                BitmapImage image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.StreamSource = stream;
+                image.EndInit();
+                return image;
+            }
+        }
+    }
+    }
+
---
+++ Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+//var listener = new Socket(AddressFamily.InterNetwork,
+//                          SocketType.Dgram,
+//                          ProtocolType.Udp);
+
+
+//var ip = IPAddress.Parse("127.0.0.1");
+//var port = 45678;
+//var listenerEP = new IPEndPoint(ip, port);
+
+//listener.Bind(listenerEP);
+
+//var buffer = new byte[ushort.MaxValue - 29];
+//EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+
+
+//var count = 0;
+//var msg = string.Empty;
+
+
+
+
+
+
+//while (true)
+//{
+//    var result = await listener.ReceiveFromAsync(new ArraySegment<byte>(buffer),
+//                                                 SocketFlags.None,
+//                                                 remoteEP);
+
+//    count = result.ReceivedBytes;
+//    msg = Encoding.Default.GetString(buffer, 0, count);
+//    Console.WriteLine($"{result.RemoteEndPoint} : {msg}");
+//}
+
+class Program
+{
+    static async Task Main()
+    {
+        Socket udpServer = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint localEndpoint = new IPEndPoint(IPAddress.Any, 12345);
+        udpServer.Bind(localEndpoint);
+        Console.WriteLine($"Server started on {localEndpoint}");
+        while (true)
+        {
+            EndPoint remoteEndpoint = new IPEndPoint(IPAddress.Any, 0);
+            byte[] receiveBuffer = new byte[1024];
+            var result = await udpServer.ReceiveFromAsync(receiveBuffer, SocketFlags.None, remoteEndpoint) ;
+            Console.WriteLine($"Received request from {remoteEndpoint}");
+
+            Bitmap screenshot = GetScreenshot();
+            byte[] imageData = ImageToByteArray(screenshot);
+            await udpServer.SendToAsync(imageData, SocketFlags.None, remoteEndpoint);
+            Console.WriteLine($"Sent screenshot to {remoteEndpoint}");
+        }
+    }
+
+    static Bitmap GetScreenshot()
+    {
+        Bitmap bitmap = new Bitmap("C:\\Users\\Admin\\Desktop\\c9dea34d94fd90eb00a809be55c9bb44.jpg");
+        return bitmap;
+        
+    }
+
+    static byte[] ImageToByteArray(Image image)
+    {
+        using (MemoryStream stream = new MemoryStream())
+        {
+            image.Save(stream, ImageFormat.Png);
+            return stream.ToArray();
+        }
+    }
+}
